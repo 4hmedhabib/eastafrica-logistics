4ac26544cdc178e7b608b97d60f8908d3421fab8
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-26 20:29:41 +0300

    updated appbar compn

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,17 +1,37 @@
-import React, { FC } from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import React, { FC } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Switch as ThemeSwitcher,
+} from "@mui/material/";
+import {
+  Menu,
+  DarkMode as DarkModeIcon,
+  LightMode as LightModeIcon,
+} from "@mui/icons-material";
 
+interface Props {
+  handleDrawerToggle: () => void;
+}
 
-const AppBarComponent: FC = (): JSX.Element => {
-    return <div>
-        <AppBar position='static'>
-            <Toolbar>
-                <Typography>
-                    eh
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    </div>;
+const AppBarComponent: FC<Props> = ({ handleDrawerToggle }): JSX.Element => {
+  return (
+    <div>
+      <AppBar position="static" variant="elevation" fle>
+        <Toolbar variant="dense">
+          <IconButton color="inherit" edge="start">
+            <Menu />
+          </IconButton>
+          <Typography variant="subtitle2" component="div">
+            Trading App
+          </Typography>
+        </Toolbar>
+        <ThemeSwitcher />
+      </AppBar>
+    </div>
+  );
 };
 
 export default AppBarComponent;
