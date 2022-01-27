2c1741c036794efd9f456a82446b9dc348b05e65
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-27 09:43:32 +0300

    updated some files

--- src/components/DataGrid/index.tsx
+++ src/components/DataGrid/index.tsx
@@ -1,7 +1,15 @@
-import { FC, useState } from "react";
-import { useTheme } from "@mui/material";
+import { FC, useEffect, useState } from "react";
+import { styled, useTheme } from "@mui/material";
 import { ColDef, RowClickedEvent, GridApi } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
 
+const GridStyledWrapper = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+  padding: theme.spacing(2, 1),
+  justifyContent: "center",
+}));
 interface DataGridProps<T> {
   showNoRowsOverlay?: boolean;
   size: { height: number | string; width: number | string };
@@ -25,7 +33,23 @@ const DataGrid = <T,>({
     setGridColumnApi(params.columnApi);
   };
 
-  return <div></div>;
+  useEffect(() => {
+    if (showNoRowsOverlay) {
+      gridApi?.showNoRowsOverlay();
+    }
+  }, []);
+
+  return (
+    <GridStyledWrapper>
+      <div
+        className={`${
+          palette.mode === "dark" ? "ag-theme-alpine-dark" : "ag-theme-alpine"
+        }`}
+      >
+        <AgGridReact></AgGridReact>
+      </div>
+    </GridStyledWrapper>
+  );
 };
 
 export default DataGrid;
