00e835b5764c9605c6d317561a44de337e6d7190
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-23 13:48:35 +0300

    deleted: old version

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AppBar } from "./AppBar/";
-export { default as SideBar } from "./SideBar/";
-export { default as Menu } from "./Menu";
-export { default as DataGrid } from "./DataGrid";
--- src/features/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Blotter } from "./Blotter";
-export { default as Dashboard } from "./Dashboard";
-export { default as TradeTicket } from "./TradeTicket";
