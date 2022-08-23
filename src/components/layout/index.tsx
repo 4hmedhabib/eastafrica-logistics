a19a795811529e7ca0eb59213e6ce5bb94ccecbc
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-23 15:03:00 +0300

    added: layout

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Header from "../head";
+
+type Props = {};
+
+export const Layout = ({}: Props) => {
+  return (
+    <Fragment>
+        <Header />
+    </Fragment>
+  );
+};
