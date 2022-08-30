e12247081f4f956c66ec91d8c5a5c93305afa84a
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-30 15:08:16 +0300

    updated: layout in components

--- src/components/layouts/general/index.js
+++ src/components/layouts/general/index.js
@@ -1,8 +1,5 @@
 import TopBar from './TopBar';
 import Header from './Header';
-import HeaderModern from './HeaderModern';
-import HeaderWidget from './HeaderWidget';
-import HeaderTransparent from './HeaderTransparent';
 import TopHeader from './TopHeader';
 import TitleHeading from './TitleHeading';
 import Footer from './Footer';
@@ -16,9 +13,6 @@ export {
     Header,
     TopHeader,
     Slider,
-    HeaderModern,
-    HeaderWidget,
-    HeaderTransparent,
     TitleHeading,
     Footer,
     BottomBar,
