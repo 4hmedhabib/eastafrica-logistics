e3dfbe21a25157da33d83083d6363ed8d6d6e048
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-08-28 18:57:22 +0300

    updated: routes api

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,29 @@
 package routes
+
+import (
+	"github.com/4hmedhabib/url_shortener_api/database"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	rdb := database.CreateClient(0)
+	defer rdb.Close()
+
+	value, err := rdb.Get(database.Ctx, url).Result()
+	if err == nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "url not found!"})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to DB",
+		})
+	}
+
+	rdbInr := database.CreateClient(1)
+	defer rdbInr.Close()
+
+	_ = rdbInr.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, 301)
+}
